449a57492d08fbff71b24aa7555821b0f7e9c269
adiljins <adiljins@gmail.com>
2022-12-12 13:08:34 -0500

    Final version: - changing spring.jpa.hibernate.ddl-auto, in order to save sql tables permanently. - with .jar file

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -11,21 +11,21 @@ import java.util.List;
 @Configuration
 public class StudentConfig {
 
-    @Bean
-    CommandLineRunner commandLineRunner(StudentRepository repository){
-        return args -> {
-            Student mariam = new Student(
-                    "Mariam",
-                    "mariamjamal@gmail.com",
-                    LocalDate.of(2000, Month.JANUARY, 5)
-            );
-            Student alex = new Student(
-                    "Alex",
-                    "alex@gmail.com",
-                    LocalDate.of(2004, Month.JANUARY, 5)
-            );
-            repository.saveAll(
-                    List.of(mariam,alex));
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository repository){
+//        return args -> {
+//            Student mariam = new Student(
+//                    "Mariam",
+//                    "mariamjamal@gmail.com",
+//                    LocalDate.of(2000, Month.JANUARY, 5)
+//            );
+//            Student alex = new Student(
+//                    "Alex",
+//                    "alex@gmail.com",
+//                    LocalDate.of(2004, Month.JANUARY, 5)
+//            );
+//            repository.saveAll(
+//                    List.of(mariam,alex));
+//        };
+//    }
 }
